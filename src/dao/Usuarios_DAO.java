0850c413a4293b6829bb23aebe1579396177c3ff
tanimotothamye <thamyetanimoto@outlook.com>
2023-10-23 14:22:55 -0300

    Alterações Atividade em sala - Query

--- src/dao/Usuarios_DAO.java
+++ src/dao/Usuarios_DAO.java
@@ -9,6 +9,7 @@ import bean.UsuariosTto;
 import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
 
 /**
@@ -60,16 +61,61 @@ public class Usuarios_DAO extends DAO_Abstract {
         session.getTransaction().commit();
         return lista;
     }
-    
-    public UsuariosTto login(String usuario, String senha){
-    session.beginTransaction();
-    Criteria criteria = session.createCriteria(UsuariosTto.class);
-    criteria.add(Restrictions.eq("nicknameTto", usuario));
-    criteria.add(Restrictions.eq("senhaTto", senha));
-    UsuariosTto usuariosLogin = (UsuariosTto) criteria.uniqueResult();
-    session.getTransaction().commit();
-    
-    return  usuariosLogin;
+
+    public UsuariosTto login(String usuario, String senha) {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(UsuariosTto.class);
+        criteria.add(Restrictions.eq("nicknameTto", usuario));
+        criteria.add(Restrictions.eq("senhaTto", senha));
+        UsuariosTto usuariosLogin = (UsuariosTto) criteria.uniqueResult();
+        session.getTransaction().commit();
+
+        return usuariosLogin;
     }
-    
+
+    public List listNomeTto(String nomeTto) {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(UsuariosTto.class);
+//      criteria.add(Restrictions.like("nome", +nome+;
+        criteria.add(Restrictions.like("nomeTto", nomeTto, MatchMode.ANYWHERE));
+        List lista = criteria.list();
+        session.getTransaction().commit();
+
+        return lista;
+
+    }
+
+    public List listCpfTto(String cpfTto) {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(UsuariosTto.class);
+//      criteria.add(Restrictions.like("nome", +nome+;
+        criteria.add(Restrictions.like("cpfTto", cpfTto, MatchMode.ANYWHERE));
+        List lista = criteria.list();
+        session.getTransaction().commit();
+
+        return lista;
+
+    }
+
+    public List listNomeCpf(String nomeTto, String cpfTto) {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(UsuariosTto.class);
+//      criteria.add(Restrictions.like("nome", +nome+;
+        criteria.add(Restrictions.like("nomeTto", nomeTto, MatchMode.ANYWHERE));
+        criteria.add(Restrictions.like("cpfTto", cpfTto, MatchMode.ANYWHERE));
+        List lista = criteria.list();
+        session.getTransaction().commit();
+
+        return lista;
+
+    }
+
+    public static void main(String[] args) {
+        Usuarios_DAO usuarios_DAO = new Usuarios_DAO();
+        List lista = usuarios_DAO.listAll();
+        for (Object usuarios : lista) {
+            System.out.println("nome:" + ((UsuariosTto) usuarios).getNomeTto());
+        }
+    }
+
 }
